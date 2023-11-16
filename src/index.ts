075f6db492f4b26deb561f219f4693e3482a9d78
Alejandro Butron <alesvoy@gmail.com>
2023-11-15 18:30:49 -0600

    Add DB and basic routing

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+
+import express, { Request } from 'express';
+import prisma from './utils/db';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const allUnits = await prisma.unit.findMany()
+  console.log(allUnits);
+  res.send('Hello World!');
+});
+
+app.post('/', async (req: Request, res) => {
+  const newUnit = await prisma.unit.create({
+    data: {
+      name: req.body.name,
+    },
+  })
+  res.json(newUnit);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
